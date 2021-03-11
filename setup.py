552e77c504521c27104ad396786dd36bf40c5b2d
zhyipeng <zhyipeng@outlook.com>
2021-03-11 12:19:25 +0800

    0.0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='zhtools',
-    version='0.0.1',
+    version='0.0.2',
     author='zhyipeng',
     author_email='zhyipeng@outlook.com',
     description='Some simple tool methods like cache, exporter and so on.',
