1b2624ec641ec47bb437d83dacfa3d7025424cd9
zhyipeng <zhyipeng@outlook.com>
2022-07-21 12:53:06 +0800

    hotfix: import cycle

--- zhtools/config.py
+++ zhtools/config.py
@@ -1,16 +1,29 @@
 import dataclasses
 import datetime
+import typing
 
-from zhtools.cache.storages import MemoryStorage, Storage
+if typing.TYPE_CHECKING:
+    from zhtools.cache import Storage
 
 __all__ = ['config']
 
 
 @dataclasses.dataclass
 class Config:
-    storage: Storage = MemoryStorage()
+    _storage: 'Storage' = None
     default_expire: int = None
     TZ: datetime.timezone = datetime.timezone.utc
+
+    @property
+    def storage(self):
+        if self._storage is None:
+            from zhtools.cache import MemoryStorage
+            self._storage = MemoryStorage()
+        return self._storage
+
+    @storage.setter
+    def storage(self, val: 'Storage'):
+        self._storage = val
 
 
 config = Config()
